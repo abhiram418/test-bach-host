8a7f427bf02a377f79a6c71e19628701ef56434b
Abhiram <abhiram.ch418@gmail.com>
2024-10-02 11:44:56 +0530

    first commit

---
+++ Controllers/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestClass : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public ActionResult Sum([FromRoute] int number)
+        {
+            return Ok(number + 1000);
+        }
+    }
+}
